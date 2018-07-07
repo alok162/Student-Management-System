b2a9d670d3d71de9c62663c95ccd19827d794a0f
Alok <alok@Aloks-MacBook-Pro.local>
2018-07-07 20:57:56 +0530

    serializing the data for the api

---
+++ student_app/serializers.py
@@ -0,0 +1,31 @@
+from student_app.models import User
+from student_app.models import Enrollment
+from rest_framework import serializers
+from django.contrib.auth.forms import UserCreationForm
+from student_app.models import User
+from django.contrib.auth.password_validation import validate_password
+
+
+class UserSignupSerializer(UserCreationForm):
+        # model serializer inbuilt method with create() and update()
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class RegisterCourseSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = fields = ['student', 'course']
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    """
+    Serializer for password change endpoint.
+    """
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate_new_password(self, value):
+        validate_password(value)
+        return value
