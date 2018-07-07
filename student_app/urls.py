26cf52d7e7b6931822ca74c6683e1336a20b8746
Alok <alok@Aloks-MacBook-Pro.local>
2018-07-07 14:34:05 +0530

    added list courses routing

--- student_app/urls.py
+++ student_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^signup/', views.User_Signup.as_view()),
     url(r'^register_course/$', views.Register_Course.as_view()),
     url(r'^delete_course/(?P<pk>[0-9]+)/$', views.Register_Course.as_view()),
+    url(r'^list_course/(?P<pk>[0-9]+)/$', views.Student_Course.as_view()),
+
 ]
