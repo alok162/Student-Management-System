e991a3e60b84154ee99c7b422ff91f44e4d9bb6d
Alok <alok@Aloks-MacBook-Pro.local>
2018-07-07 19:45:40 +0530

    api to list all the registred courses to the particular student

--- student_app/views.py
+++ student_app/views.py
@@ -5,11 +5,16 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
-from student_app.models import Enrollment
+from student_app.models import Enrollment, Courses
 
 from student_app.serializers import UserSignupSerializer
 from student_app.serializers import RegisterCourseSerilizer
 from student_app.models import User
+from student_app.serializers import ChangePasswordSerializer
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from rest_framework import permissions
+from django.db.models import Q
 
 
 class User_Signup(APIView):
@@ -22,13 +27,20 @@ class User_Signup(APIView):
         return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+
 class Register_Course(APIView):
     def post(self, request, format=None):
         data = JSONParser().parse(request)
         serializer = RegisterCourseSerilizer(data=data)
+        print('my json data', data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            total_subject = Enrollment.objects.filter(
+                student=data['student']).count()
+            print('total subject', total_subject)
+            if total_subject < 5:
+                serializer.save()
+                return Response('successfully registered in your account', status=status.HTTP_201_CREATED)
+            return Response('Your can not register more than 5 subjects')
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
@@ -36,4 +48,15 @@ class Register_Course(APIView):
         enroll_obj.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+class Student_Course(APIView):
+    def get(self, request, pk, format=None):
+        data = Enrollment.objects.filter(student=pk).select_related('course')
+        res_data = []
+        for i in data:
+            res = {}
+            res['enrollment_id'] = i.id
+            res['course_name'] = i.course.course_name
+            res_data.append(res)
+        return Response(res_data)
 
