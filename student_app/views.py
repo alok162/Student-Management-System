1b396bf7188b106f67035ea33472d032865aee9f
Alok <alok@Aloks-MacBook-Pro.local>
2018-07-07 14:06:17 +0530

    api for adding course for particular student

--- student_app/views.py
+++ student_app/views.py
@@ -19,3 +19,13 @@ class User_Signup(APIView):
             form.save()
             return Response(request.data, status=status.HTTP_201_CREATED)
         return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Register_Course(APIView):
+    def post(self, request, format=None):
+        data = JSONParser().parse(request)
+        serializer = RegisterCourseSerilizer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
