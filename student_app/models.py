0d915129467b4be3b2ee782054de36f101cc3ea4
Alok <alok@Aloks-MacBook-Pro.local>
2018-07-07 12:11:30 +0530

    created custom user model

--- student_app/models.py
+++ student_app/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+
 class Courses(models.Model):
     course_name = models.CharField(max_length=200)
+
 
 class Enrollment(models.Model):
     course = models.ForeignKey(Courses)
     student = models.ForeignKey(User)
-
-
